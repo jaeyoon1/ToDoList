b313be145d0dd0a6b349d08a270ba8640eb567a2
ca25nada <ca25nada@naver.com>
2014-09-21 17:45:11 -0600

    Placeholder methods for TaskList

--- ToDoList/src/jaeyoon1/ualberta/c350/todolist/TaskList.java
+++ ToDoList/src/jaeyoon1/ualberta/c350/todolist/TaskList.java
@@ -1,5 +1,32 @@
 package jaeyoon1.ualberta.c350.todolist;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jaeyoon1.ualberta.c350.todolist.Task;
+
 public class TaskList {
+
+	public Collection<Task> getTasks() {
+		// TODO Auto-generated method stub
+		return new ArrayList<Task>();
+	}
+
+	public void addTask(Task sampleTask) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void removeTask(){
+		
+	}
+	
+	public void archiveTask(){
+		
+	}
+	
+	public void selectTask(){
+		
+	}
 
 }
